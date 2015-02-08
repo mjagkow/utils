293b8048ab166f5fd5d8ab3c93fc3f4c7d366e00
Igor Myagkov <igor.myagkov@cern.ch>
2015-02-09 01:15:40 +0300

    Moving towards parsing options with argp

--- option_helper/example.cpp
+++ option_helper/example.cpp
@@ -1,74 +1,95 @@
 /* 
- * File:   example.cpp
- * Author: Igor Myagkov - myagkov@physics.msu.ru
+ * File:   main.cpp
+ * Author: Igor Myagkov - igor.myagkov@cern.ch
  *
- * Created on August 14, 2014, 7:23 PM
+ * Created on February 5, 2015, 5:25 PM
  */
- 
-// Compile with
-// g++ -o example example.cpp option_helper.cpp -std=c++11 -I./ -lboost_program_options
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include "option_helper.h"
-
-
+#include <cstdlib>
+#include <argp.h>
 
 using namespace std;
 
-string version("1.0");
+const char *argp_program_version = "Option example 1.0";
+
+static struct argp_option options[] = {
+    {"verbose",  'v', 0,      0,  "Produce verbose output" },
+    {"quiet",    'q', 0,      0,  "Don't produce any output" },
+    {"silent",   's', 0,      OPTION_ALIAS },
+    {"output",   'o', "FILE", 0,  "Output to FILE instead of standard output" },
+    { 0 }
+};
+
+/*
+ * Try Also --help and --version !
+ */
+
+struct arguments
+{
+    char *args[2];                /* arg1 & arg2 */
+    int silent, verbose;
+    char *output_file;
+};
+
+static error_t
+parse_opt (int key, char *arg, struct argp_state *state)
+{
+    // Retrieving current configuration
+    struct arguments *arguments = (struct arguments*)state->input;
+
+    switch (key)
+    {
+    case 'q': case 's':
+        arguments->silent = 1;
+        break;
+    case 'v':
+        arguments->verbose = 1;
+        break;
+    case 'o':
+        arguments->output_file = arg;
+        break;
+
+    case ARGP_KEY_ARG:
+        // Normal, non-key argument
+        if (state->arg_num >= 2) /* Too many arguments. */
+            argp_usage (state);
+        arguments->args[state->arg_num] = arg;
+        break;
+        
+    case ARGP_KEY_END:
+        // End of argument stream
+        if (state->arg_num < 2) /* Not enough arguments. */
+            argp_usage (state);
+        break;
+
+    default:
+        return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+static struct argp argp = {options, parse_opt};
 
 
 int main(int argc, char** argv) {
-    
-    OptionHelper opt;
-    
-    string mode;
-    
-    opt.addVisible()("help,h", "Display help message");
-    opt.addVisible()("version,v", "Display program version");
-    opt.addVisible()("mode", OptionHelper::value<string>(&mode), "Set program mode; available options: mode1, mode2, mode3");
-    opt.addVisible()("compression,c", OptionHelper::value<int>()->default_value(1), "Set compression level");
-    
-    opt.addHidden()("config", OptionHelper::value< vector<string> >(), "List of configuration files");
-    opt.setPositional("config", -1);
-    
-    opt.parse(argc, argv);
-    
+    // Arbitrary structure to store configuration
+    struct arguments arguments;
 
-    if(opt.isSet("help"))
-    {
-        cout << 
-            "\nOption helper usage example\n\n" <<
-            "Usage: example [options] [config, ...]\n\n" <<
-            opt << '\n';
-        return 0;
-    }
+    // Default values
+    arguments.silent = 0;
+    arguments.verbose = 0;
+    arguments.output_file = (char *)"-";
     
-
-    if(opt.isSet("version"))
-    {
-        cout << version << '\n';
-        return 0;
-    }
+    argp_parse(&argp, argc, argv, 0, 0, (void *)&arguments);
     
+    printf ("ARG1 = %s\nARG2 = %s\nOUTPUT_FILE = %s\n"
+            "VERBOSE = %s\nSILENT = %s\n",
+            arguments.args[0], arguments.args[1],
+            arguments.output_file,
+            arguments.verbose ? "yes" : "no",
+            arguments.silent  ? "yes" : "no");
 
-    if(opt.isSet("mode"))
-        cout << "Mode: " << mode << '\n';
-
-
-    if(opt.isSet("compression"))
-        cout << "Compression level: " << opt["compression"].as<int>() << '\n';
-    
- 
-    if(opt.isSet("config"))
-    {
-        cout << "\nConfiguration files:\n";
-        for (auto conf : opt["config"].as< vector<string> >())
-            cout << '\t' << conf << '\n';
-    }
-    
-
-    cout << '\n' << opt.options().size() << " kinds of options had been set\n";
+    return 0;
 }
+
+
